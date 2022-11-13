27c551159e66c54b73d0cbee10dab9ae2cfe8d66
syndersage <syndersage@gmail.com>
2022-11-14 05:45:57 +0700

    DH implementation + GUI

--- src/main/java/cryptography/asymmetric/DH/DH.java
+++ src/main/java/cryptography/asymmetric/DH/DH.java
@@ -3,7 +3,10 @@ package cryptography.asymmetric.DH;
 import java.math.BigInteger;
 
 public class DH {
-  public static byte[] calculate(byte[] base, byte[] modulus, byte[] privateKey) {
+  public static byte[] calculate(DHKeys keys) {
+    byte[] base = keys.base;
+    byte[] modulus = keys.modulus;
+    byte[] privateKey = keys.privateKey;
     BigInteger intBase = new BigInteger(base);
     BigInteger intModulus = new BigInteger(modulus);
     BigInteger intKey = new BigInteger(privateKey);
--- src/main/java/cryptography/asymmetric/DH/DHKeys.java
+++ src/main/java/cryptography/asymmetric/DH/DHKeys.java
@@ -7,17 +7,60 @@ public class DHKeys {
 
   public byte[] base;
   public byte[] modulus;
+  public byte[] privateKey;
+
+  public static final BigInteger MIN_BASE = new BigInteger("2");
+  public static final BigInteger MIN_MODULUS = new BigInteger("16");
 
   public DHKeys(int keyLength) throws IllegalArgumentException {
-    BigInteger tempBase;
+    if (keyLength < 4 | keyLength > 3072) {
+      throw new IllegalArgumentException("Too " + (keyLength < 4 ? "small" : "big") + " key length");
+    }
+    BigInteger tempBase, tempModulus, subGroup;
     do {
-      tempBase = Numbers.genNumber(keyLength - 1);
-      while (!Numbers.isPrime(tempBase)) {
-        tempBase = Numbers.genNumber(keyLength - 1);
-      }
-      modulus = tempBase.multiply(BigInteger.TWO).add(BigInteger.ONE).toByteArray();
-      System.out.println(Numbers.isPrime(new BigInteger(modulus)));
-    } while (modulus.length != keyLength);
+      subGroup = new BigInteger(keyLength, Numbers.random);
+    } while (!subGroup.isProbablePrime(128));
+    BigInteger multiplier;
+    do {
+      multiplier = new BigInteger(32, Numbers.random);
+      tempModulus = subGroup.multiply(multiplier).add(BigInteger.ONE);
+    } while (!tempModulus.isProbablePrime(128));
+    do {
+      tempBase = new BigInteger(32, Numbers.random).modPow(multiplier, tempModulus);
+    } while (tempBase.equals(BigInteger.ONE));
     base = tempBase.toByteArray();
+    modulus = tempModulus.toByteArray();
+  }
+
+  public DHKeys(BigInteger base, BigInteger modulus) {
+    if (base.compareTo(new BigInteger("1")) <= 0) {
+      throw new IllegalArgumentException(" Base cannot be less than 2");
+    }
+    if (modulus.compareTo(new BigInteger("15")) <= 0) {
+      throw new IllegalArgumentException(" Modulus cannot be less than 16");
+    }
+    System.out.println("BASE: " + base);
+    System.out.println("MODU: " + modulus);
+    if (base.compareTo(modulus) >= 0) {
+      throw new IllegalArgumentException("Base size have to be smaller than modulus");
+    }
+    this.base = base.toByteArray();
+    this.modulus = modulus.toByteArray();
+  }
+
+  public void setPrivateKey(BigInteger key) {
+    if (key.compareTo(BigInteger.ONE) <= 0) {
+      throw new IllegalArgumentException(" Private ey cannot be less than 1");
+    }
+    if (key.compareTo(new BigInteger(modulus)) >= 0) {
+      throw new IllegalArgumentException(" Private key size have to be smaller than modulus");
+    }
+    this.privateKey = key.toByteArray();
+  }
+
+  public void setPrivateKey() {
+    do {
+      privateKey = new BigInteger(modulus.length * 8, Numbers.random).toByteArray();
+    } while (new BigInteger(privateKey).compareTo(new BigInteger(modulus)) >= 0 | new BigInteger(privateKey).compareTo(BigInteger.ONE) <= 0);
   }
 }
