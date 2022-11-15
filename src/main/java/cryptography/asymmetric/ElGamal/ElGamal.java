28e9f65b4e4ed22827477a095be91aacfaadf351
syndersage <syndersage@gmail.com>
2022-11-16 05:16:57 +0700

    ElGamal completed + GUI

--- src/main/java/cryptography/asymmetric/ElGamal/ElGamal.java
+++ src/main/java/cryptography/asymmetric/ElGamal/ElGamal.java
@@ -1,43 +1,74 @@
 package cryptography.asymmetric.ElGamal;
 
+import cryptography.asymmetric.GUI.UserSelections;
 import cryptography.asymmetric.Numbers;
+import cryptography.asymmetric.RSA.OAEP;
 import java.math.BigInteger;
 import java.util.Arrays;
 
 public class ElGamal {
-  public static byte[] encrypt(byte[] data, ElGamalKeys keys) {
-    System.out.println(keys.modulus.length);
+  public static byte[] encrypt(byte[] data, ElGamalKeys keys, OAEP paddingParams) {
     BigInteger randomValue, tempModulus = new BigInteger(keys.modulus);
     do {
       randomValue = new BigInteger(tempModulus.bitLength(), Numbers.random);
     } while (randomValue.compareTo(tempModulus.subtract(BigInteger.TWO)) >= 0);
     BigInteger tempGenerator = new BigInteger(keys.base);
     byte[] encryptedGenerator = Numbers.i2osp(tempGenerator.modPow(randomValue, tempModulus),
-        keys.modulus.length);
+        keys.byteKeyLength);
     BigInteger tempPublic = new BigInteger(keys.personalPublicKey);
-    byte[][] splittedData = Numbers.splitArray(data, keys.modulus.length - 1);
-    byte[][] encryptedMessageChunks = new byte[splittedData.length][];
-    for (int i = 0; i < splittedData.length; i++) {
-      System.out.println(i);
-      encryptedMessageChunks[i] = Numbers.i2osp(new BigInteger(1, splittedData[i]).multiply(tempPublic.modPow(randomValue, tempModulus)).mod(tempModulus),
-          keys.modulus.length);
+    int chunkSize;
+    switch (paddingParams.padding) {
+      case "PKCS#1-OAEP" -> {
+        int bytesForPadding = 2 * UserSelections.digest.getDigestLength() + 2;
+        chunkSize = keys.byteKeyLength - bytesForPadding;
+        if (chunkSize <= 0) {
+          throw new IllegalArgumentException(" Key size for PKCS#1-OAEP must be at least: " + (bytesForPadding * 8 + 1) + " bits");
+        }
+      }
+      case "None" -> chunkSize = keys.byteKeyLength - 1;
+      default -> throw new IllegalArgumentException(" Invalid padding name: " + paddingParams.padding);
     }
-    //byte[] encryptedMessage = new BigInteger(1, data).multiply(tempPublic.pow(randomValue, tempModulus)).mod(tempModulus).toByteArray();
+    byte[][] splittedData = Numbers.splitArray(data, chunkSize);
+    byte[][] encryptedMessageChunks = new byte[splittedData.length][];
+    byte[] messageChunk;
+    UserSelections.progress.setMaximum(encryptedMessageChunks.length);
+    for (int i = 0; i < splittedData.length; i++) {
+      if (UserSelections.calculationThread.isCancelled()) {
+        return new byte[0];
+      }
+      UserSelections.progress.setValue(i);
+      switch (paddingParams.padding) {
+        case "PKCS#1-OAEP" -> messageChunk = OAEP.wrap(splittedData[i], paddingParams);
+        case "None" -> messageChunk = splittedData[i];
+        default -> throw new IllegalArgumentException("Invalid padding name");
+      }
+      encryptedMessageChunks[i] = Numbers.i2osp(Numbers.os2ip(messageChunk).multiply(tempPublic.modPow(randomValue, tempModulus)).mod(tempModulus),
+          keys.byteKeyLength);
+    }
     return Numbers.concatArrays(encryptedGenerator, Numbers.convert2Dto1D(encryptedMessageChunks));
   }
 
-  public static byte[] decrypt(byte[] data, ElGamalKeys keys) {
+  public static byte[] decrypt(byte[] data, ElGamalKeys keys, OAEP paddingParams) {
     BigInteger tempModulus = new BigInteger(keys.modulus);
-    BigInteger encryptedGenerator = Numbers.os2ip(Arrays.copyOf(data, keys.modulus.length));
-    byte[][] encryptedMessageChunks = Numbers.splitArray(Arrays.copyOfRange(data, keys.modulus.length, data.length), keys.modulus.length);
-    byte[][] decryptedMessageChunks = new byte[encryptedMessageChunks.length][];
+    BigInteger encryptedGenerator = Numbers.os2ip(Arrays.copyOf(data, keys.byteKeyLength));
+    byte[][] splittedData = Numbers.splitArray(Arrays.copyOfRange(data, keys.byteKeyLength, data.length), keys.byteKeyLength);
+    byte[][] decryptedData = new byte[splittedData.length][];
     BigInteger tempPrivate = new BigInteger(keys.personalPrivateKey);
-    for (int i = 0; i < encryptedMessageChunks.length; i++) {
-      decryptedMessageChunks[i] = Numbers.i2osp(new BigInteger(1, encryptedMessageChunks[i]).multiply(encryptedGenerator.modPow(tempModulus.subtract(BigInteger.ONE).subtract(tempPrivate), tempModulus)).mod(tempModulus), keys.modulus.length);
+    BigInteger paddedChunk;
+    UserSelections.progress.setMaximum(decryptedData.length);
+    for (int i = 0; i < decryptedData.length; i++) {
+      if (UserSelections.calculationThread.isCancelled()) {
+        return new byte[0];
+      }
+      UserSelections.progress.setValue(i);
+      paddedChunk = Numbers.os2ip(splittedData[i]);
+      decryptedData[i] = Numbers.i2osp(paddedChunk.multiply(encryptedGenerator.modPow(tempModulus.subtract(BigInteger.ONE).subtract(tempPrivate), tempModulus)).mod(tempModulus),
+          keys.byteKeyLength);
+      switch (paddingParams.padding) {
+        case "PKCS#1-OAEP" -> decryptedData[i] = OAEP.unwrap(decryptedData[i], paddingParams);
+        case "None" -> decryptedData[i] = Arrays.copyOfRange(decryptedData[i], 1, keys.byteKeyLength);
+      }
     }
-    for (int i = 0; i < decryptedMessageChunks.length; i++) {
-      decryptedMessageChunks[i] = Arrays.copyOfRange(decryptedMessageChunks[i], 1, decryptedMessageChunks[i].length);
-    }
-    return Numbers.convert2Dto1D(decryptedMessageChunks);
+    return Numbers.convert2Dto1D(decryptedData);
   }
 }
--- src/main/java/cryptography/asymmetric/ElGamal/ElGamalKeys.java
+++ src/main/java/cryptography/asymmetric/ElGamal/ElGamalKeys.java
@@ -8,7 +8,7 @@ public class ElGamalKeys {
 
   public byte[] modulus;
 
-  public int keyLength;
+  public int byteKeyLength;
   public byte[] base;
   public byte[] personalPublicKey;
 
@@ -19,20 +19,52 @@ public class ElGamalKeys {
 
   public ElGamalKeys(int keyLength) throws IllegalArgumentException {
     if (keyLength < MIN_KEY_LENGTH | keyLength > MAX_KEY_LENGTH) {
-      throw new IllegalArgumentException("Key length (in bites) have to in range from " + MIN_KEY_LENGTH + " to " + MAX_KEY_LENGTH);
+      throw new IllegalArgumentException("Key length (in bits) have to in range from " + MIN_KEY_LENGTH + " to " + MAX_KEY_LENGTH);
     }
     Map<String, BigInteger> group = Numbers.generateCyclicGroup(keyLength);
     modulus = group.get("Modulus").toByteArray();
     base = group.get("Generator").toByteArray();
-    this.keyLength = keyLength;
+    this.byteKeyLength = modulus.length;
+  }
+
+  public ElGamalKeys(BigInteger base, BigInteger modulus) throws IllegalArgumentException {
+    if (modulus.bitLength() < MIN_KEY_LENGTH | modulus.bitLength() > MAX_KEY_LENGTH + Numbers.randomValueSize + 1) {
+      throw new IllegalArgumentException(" Elgamal key length min and max length respectively: " + MIN_KEY_LENGTH + ", " + MAX_KEY_LENGTH);
+    }
+    if (base.compareTo(BigInteger.ONE) <= 0 | base.compareTo(modulus) >= 0) {
+      throw new IllegalArgumentException(" Generator must be positive and less than modulus");
+    }
+    this.base = base.toByteArray();
+    this.modulus = modulus.toByteArray();
+    this.byteKeyLength = this.modulus.length;
   }
 
   public void setPersonalKeys() {
     BigInteger tempPrivate;
     do {
-      tempPrivate = new BigInteger(keyLength, Numbers.random);
+      tempPrivate = new BigInteger(byteKeyLength * 8, Numbers.random);
     } while (tempPrivate.compareTo(new BigInteger(modulus).add(BigInteger.ONE)) >= 0);
     personalPublicKey = new BigInteger(base).modPow(tempPrivate, new BigInteger(modulus)).toByteArray();
     personalPrivateKey = tempPrivate.toByteArray();
+  }
+
+  public void setPersonalKeys(BigInteger publicKey, BigInteger privateKey) throws IllegalArgumentException {
+    if (publicKey.compareTo(BigInteger.ONE) <= 0 | publicKey.compareTo(new BigInteger(modulus)) >= 0
+      | privateKey.compareTo(BigInteger.ONE) <= 0 | privateKey.compareTo(new BigInteger(modulus)) >= 0) {
+      throw new IllegalArgumentException(" Keys must be greater than 1 and less than modulus");
+    }
+    personalPublicKey = publicKey.toByteArray();
+    personalPrivateKey = privateKey.toByteArray();
+  }
+
+  public void setPersonalKeys(BigInteger personalKey, boolean isPublicKey) throws IllegalArgumentException {
+    if (personalKey.compareTo(BigInteger.ONE) <= 0 | personalKey.compareTo(new BigInteger(modulus)) >= 0) {
+      throw new IllegalArgumentException((isPublicKey ? " Public" : "Private") + " key must be greater than 1 and less than modulus");
+    }
+    if (isPublicKey) {
+      personalPublicKey = personalKey.toByteArray();
+    } else {
+      personalPrivateKey = personalKey.toByteArray();
+    }
   }
 }
