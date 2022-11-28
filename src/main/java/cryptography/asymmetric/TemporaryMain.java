1f539d58f2133f34060198c5b54a73b01bef4269
syndersage <syndersage@gmail.com>
2022-11-28 11:48:27 +0700

    CLI test class update for DSA

--- src/main/java/cryptography/asymmetric/TemporaryMain.java
+++ src/main/java/cryptography/asymmetric/TemporaryMain.java
@@ -1,37 +1,32 @@
 package cryptography.asymmetric;
 
+import cryptography.asymmetric.dsa.DSA;
+import cryptography.asymmetric.dsa.DSAKeys;
+import cryptography.asymmetric.gui.UserSelections;
 import cryptography.asymmetric.md5.MD5;
+import cryptography.asymmetric.sha.SHA;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.Security;
+import java.util.Arrays;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class TemporaryMain {
-
-  public static BigInteger base;
-  public static BigInteger modulus;
-  public static BigInteger subgroup1;
-  public static byte[] privateKey;
 
   public static void main(String[] args) {
     Security.addProvider(new BouncyCastleProvider());
     double startTime = System.nanoTime();
 
-    MD5 md5 = new MD5();
-    String data = "012345678901234567890123456789012345678901234567890123456789";
-    byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
-    try {
-      bytes = Files.readAllBytes(Path.of("F:\\Пользователи\\Pavel\\OneDrive\\Рабочий стол\\Уроки\\4 курс.rar"));
-      System.out.println(bytes.length);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    md5.calculate(bytes);
+    DSAKeys key = new DSAKeys(1024, 160);
+    key.setPersonalKeys();
+    byte[] data = "hello".getBytes(StandardCharsets.UTF_8);
+    byte[] sign = DSA.sign(data, key);
+    System.out.println(Arrays.toString(sign));
+    System.out.println(Arrays.toString(DSA.verify(data, key, sign)));
 
     System.out.println("Time: " + (System.nanoTime() - startTime) / 1_000_000_000);
   }
-
 }
